505a5506eee3bea2b971f2cb5490ddfe19360067
seven332 <seven332@163.com>
2023-07-08 22:48:22 +0800

    test more avif image

--- src/avif-sample-images.test.ts
+++ src/avif-sample-images.test.ts
@@ -63,6 +63,35 @@ describe("avif-sample-images.test", () => {
         expect(image.width).toBe(file.includes("odd-width") ? 1203 : 1204);
         expect(image.height).toBe(file.includes("odd-height") ? 799 : 800);
       });
+    } else if (file.startsWith("hato") && file.endsWith("avif")) {
+      it(file, async () => {
+        const image = await decode(file);
+        expect(image.width).toBe(
+          file.includes("10bpc") || file.includes("12bpc") ? 3078 : 3082
+        );
+        expect(image.height).toBe(2048);
+      });
+    } else if (file.startsWith("plum-blossom-large") && file.endsWith("avif")) {
+      it(file, async () => {
+        const image = await decode(file);
+        expect(image.width).toBe(2048);
+        expect(image.height).toBe(2048);
+      });
+    } else if (file.startsWith("plum-blossom-small") && file.endsWith("avif")) {
+      it(file, async () => {
+        const image = await decode(file);
+        expect(image.width).toBe(128);
+        expect(image.height).toBe(128);
+      });
+    } else if (
+      file.startsWith("red-at-12-oclock-with-color") &&
+      file.endsWith("avif")
+    ) {
+      it(file, async () => {
+        const image = await decode(file);
+        expect(image.width).toBe(800);
+        expect(image.height).toBe(800);
+      });
     }
   });
 });
